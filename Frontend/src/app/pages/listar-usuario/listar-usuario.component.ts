fe75abaf2401cf4353e3c8ae1ba63ceb7cc3a1ce
David Idrobo <davididrobob@gmail.com>
2021-10-25 17:50:29 -0500

    commit listarUsuarios

--- Frontend/src/app/pages/listar-usuario/listar-usuario.component.ts
+++ Frontend/src/app/pages/listar-usuario/listar-usuario.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, HostBinding,OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { GestionarUsuarioService } from 'src/app/services/gestionar-usuario.service';
 
 
 @Component({
@@ -9,14 +10,37 @@ import { Router } from '@angular/router';
 })
 export class ListarUsuarioComponent implements OnInit {
 
- 
+  @HostBinding('class') classes = 'row';
 
-
+  usuarios: any = [];
+  constructor(private gestionarUsuarioService: GestionarUsuarioService) { }
 
   ngOnInit(): void {
+    this.getUsuarios();
   }
 
+  getUsuarios(){
+    this.gestionarUsuarioService.getUsuarios().subscribe(
+      res => {
+        this.usuarios = res;
+      },
+      err => console.error(err)
+    );
+  }
 
+  deleteUsuario(id:any){
+    this.gestionarUsuarioService.deleteUsuario(id).subscribe(
+      res => {
+        console.log(res);
+        this.getUsuarios();
+       },
+        err => console.log(err)
+      )
+  }
+
+  editUsuario(id:any){
+
+  }
 
 }
 
